bea7039a1bd44fb120fa0339e1276956bbecff1b
FBM2 <isaacjmdienstag@gmail.com>
2020-08-25 12:49:32 -0700

    test

--- MadLibs.java
+++ MadLibs.java
@@ -3,6 +3,8 @@ public class MadLibs {
 
 	public static void main(String[] args) {
 		//0. Give variable names for each of the arraylists below and create Madlibs stories.
+		//test
+
 		ArrayList<String> nouns = new ArrayList<String>();
 		ArrayList<String> verbs = new ArrayList<String>();
 		
