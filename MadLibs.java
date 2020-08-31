66b259e68efe0ff2cc0a9841352e952ddbb686c1
Tyler-Weigand <tweigand1@hwemail.com>
2020-08-30 18:57:53 -0700

    Update MadLibs.java

--- MadLibs.java
+++ MadLibs.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class MadLibs {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		//0. Give variable names for each of the arraylists below and create Madlibs stories.
 		ArrayList<String> nouns =  new ArrayList<String>();
 		ArrayList<String> verbs =  new ArrayList<String>();
@@ -23,21 +23,16 @@ public class MadLibs {
 			current = nounBuffer.readLine();
 		}
 		nounBuffer.close();
-		
 		//2. Read a verbs.txt file and store its list of verbs into an arraylist.
 
-		catch (Exception ex)
-
-		BufferedReader br2 = new BufferedReader (new FileReader (verbs.txt));
+		BufferedReader br2 = new BufferedReader (new FileReader ("verbs.txt"));
 		String verb = br2.readLine();
-		while (verb!=null);
-
+		while (verb!=null)
 		{
 			verbs.add(verb);
 			verb = br2.readLine();
 		}
 		br2.close();
-
 		//3. Read an adjectives.txt file and store its list of adjectives into an arraylist.
 
 		File file3 = new File("adjectives.txt");
@@ -47,8 +42,7 @@ public class MadLibs {
 		while ((st3 = br3.readLine()) != null) {
 			adjs.add(st3);
 		}
-		br3.close()
-
+		br3.close();
 		//4. Read an adverbs.txt file and store its list of adverbs into an arraylist.
 		File file4 = new File ("adverbs.txt");
 		BufferedReader br4 = new BufferedReader (new FileReader (file4));
@@ -58,7 +52,6 @@ public class MadLibs {
 			advs.add(st4);
 		}
 		br4.close ();
-
 		//5. Prompt user for the name of a file containing a MadLibs story.
 		// Read the story from that file and write it to System.out, but replacing each instance of
 		// <<NOUN>>, <<VERB>>, etc. with a randomly chosen word from the corresponding list.
@@ -71,7 +64,7 @@ public class MadLibs {
 		int inputCharNum = storyReader.read();
 		while(inputCharNum != -1) {
 			story += (char)inputCharNum;
-			inputCharNum = reader.read();
+			inputCharNum = storyReader.read();
 		}
 		storyReader.close();
 
@@ -81,9 +74,9 @@ public class MadLibs {
 		int advRandom = (int)(Math.random()*advs.size());
 
 		String randomNoun = nouns.get(nounRandom);
-		String randomVerb = nouns.get(verbRandom);
-		String randomAdj = nouns.get(adjRandom);
-		String randomAdv = nouns.get(advRandom);
+		String randomVerb = verbs.get(verbRandom);
+		String randomAdj = adjs.get(adjRandom);
+		String randomAdv = advs.get(advRandom);
 
 		int nounLoc = story.indexOf("<<NOUN>>");
 		story = story.substring(0, nounLoc) + randomNoun + story.substring(nounLoc + 8);
