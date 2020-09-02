16ab4984b435b57928383fde464d9731d3d75b0b
Kosi Nwabueze <kosinwabueze@gmail.com>
2020-09-02 09:26:27 -0700

    feat(all): Implemented prompting story

--- MadLibs.java
+++ MadLibs.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class MadLibs {
@@ -9,10 +10,16 @@ public class MadLibs {
 	public static void main(String[] args) {
 		//0. Give variable names for each of the arraylists below and create Madlibs stories.
 		ArrayList<String> nouns = new ArrayList<String>();
+		ArrayList<String> verbs = new ArrayList<String>();
+		ArrayList<String> adjectives = new ArrayList<String>();
+		ArrayList<String> adverbs = new ArrayList<String>();
 
 		
 		//1. Read a nouns.txt file and store its list of nouns into an arraylist.
-		nouns = readAndStoreNouns("MadlibsTestFile.txt");
+		nouns = readAndStore("nouns.txt");
+		adverbs = readAndStore("nouns.txt");
+		adjectives = readAndStore("nouns.txt");
+		verbs = readAndStore("nouns.txt");
 		
 		System.out.println(nouns);
 		
@@ -25,15 +32,57 @@ public class MadLibs {
 		//5. Prompt user for the name of a file containing a MadLibs story.
 		// Read the story from that file and write it to System.out, but replacing each instance of
 		// <<NOUN>>, <<VERB>>, etc. with a randomly chosen word from the corresponding list.
-		
-		
-		
-		
-		
-
+		buildStory(nouns, verbs, adjectives, adverbs);
 	}
+	
+	private static void buildStory(ArrayList<String> nouns,
+	 	ArrayList<String> verbs,
+		ArrayList<String> adjectives,
+		ArrayList<String> adverbs) {
+			Scanner sc = new Scanner(System.in);
 
-	private static ArrayList<String> readAndStoreNouns(String fileName) {
+			System.out.print("Enter mad libs text file> ");
+			String filename = sc.nextLine();
+
+			try {
+				BufferedReader br = new BufferedReader(new FileReader(new File(filename)));
+				
+				String line = "";
+				while ((line = br.readLine()) != null) {
+					for (String word : line.split(" ")) {
+
+						if (word.contains("<<NOUN>>")) {
+							int rn = (int)(Math.random() * nouns.size());
+							word = word.replace("<<NOUN>>", nouns.get(rn));
+						}
+
+						else if (word.contains("<<VERB>>")) {
+							int rn = (int)(Math.random() * verbs.size());
+							word = word.replace("<<VERB>>", verbs.get(rn));
+						}
+
+						else if (word.contains("<<ADJECTIVE>>")) {
+							int rn = (int)(Math.random() * adjectives.size());
+							word = word.replace("<<ADJECTIVE>>", adjectives.get(rn));
+						}
+
+						else if (word.contains("<<ADVERB>>")) {
+							int rn = (int)(Math.random() * adverbs.size());
+							word = word.replace("<<ADVERB>>", adverbs.get(rn));
+						}
+
+						System.out.print(word);
+						System.out.print(" ");
+					}
+
+					System.out.println();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	private static ArrayList<String> readAndStore(String fileName) {
 		ArrayList<String> list = new ArrayList<String>();
 		
 		File nounsFile = new File(fileName);
